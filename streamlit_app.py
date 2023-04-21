4bff09e18273d407d8af9c6b0bf4585a0d415dfa
--global <--global>
2023-04-20 21:46:13 -0400

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import mymodel as m
+# import mymodel as m
 
 st.write("""
 # Expenses
 """)
 
-st.write(m.run(window=15))
+# st.write(m.run(window=15))
 
 window = st.slider("Test Slider")
-st.write(m.run(window=window))
+# st.write(m.run(window=window))
